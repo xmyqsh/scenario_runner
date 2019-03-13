c0e993f5d964d309e45e48598dbbf161c2ee13b6
Felipe Codevilla <codevilf@bart14.server.mila.quebec>
2019-03-12 20:37:40 -0400

    added the parsing part of the scenario runner

--- srunner/challenge/utils/route_configuration_parser.py
+++ srunner/challenge/utils/route_configuration_parser.py
@@ -7,13 +7,18 @@ import xml.etree.ElementTree as ET
 """
 
 
+
+
+
+
+
 def parse_annotations_file(annotation_filename):
     # Return the annotations of which positions where the scenarios are going to happen.\
 
     with open(annotation_filename, 'r') as f:
         annotation_dict = json.loads(f.read())
 
-    # Todo, add checks for file structure erros
+    # Todo, add checks for file structure errors, such as weird names and things
 
 
     return annotation_dict
@@ -36,15 +41,8 @@ def parse_routes_file(route_filename):
         route_id = route.attrib['id']
         waypoint_list = []  # the list of waypoints that can be found on this route
         for waypoint in route.iter('waypoint'):
-            waypoint_dict = {
-                'x': waypoint.attrib['x'],
-                'y': waypoint.attrib['y'],
-                'z': waypoint.attrib['z'],
-                'pitch': waypoint.attrib['pitch'],
-                'yaw': waypoint.attrib['yaw']
-            }
 
-            waypoint_list.append(waypoint_dict)
+             waypoint_list.append(waypoint)  # Waypoints is basically a list of XML nodes
 
         list_route_descriptions.append( {
                                     'id': route_id,
@@ -67,6 +65,19 @@ def create_location_waypoint(location):
 
     }
 
+def remove_redundancy(list_of_vehicles):
+    """
+       We have a redundant vec of dics. Eliminate it for now.
+    """
+
+    vehicle_dict = {}
+
+    for mono_dict in list_of_vehicles:
+        vehicle_dict.update(mono_dict)
+
+    return vehicle_dict
+
+
 
 def scan_route_for_scenarios(route_description, world_annotations):
 
@@ -88,15 +99,15 @@ def scan_route_for_scenarios(route_description, world_annotations):
             route_description:
         """
 
-        def match_waypoints(w1, w2):
-            dx = w1['x'] - float(w2['x'])
-            dy = w1['y'] - float(w2['y'])
-            dz = w1['z'] - float(w2['z'])
+        def match_waypoints(w1, wnode):
+            dx = w1['x'] - float(wnode.attrib['x'])
+            dy = w1['y'] - float(wnode.attrib['y'])
+            dz = w1['z'] - float(wnode.attrib['z'])
 
             dist_position = math.sqrt(dx * dx + dy * dy + dz * dz)
 
-            dyaw = w1['yaw'] - float(w2['yaw'])
-            dpitch = w1['pitch'] - float(w2['pitch'])
+            dyaw = w1['yaw'] - float(wnode.attrib['yaw'])
+            dpitch = w1['pitch'] - float(wnode.attrib['pitch'])
 
             dist_angle = math.sqrt(dyaw * dyaw + dpitch * dpitch)
 
@@ -126,21 +137,24 @@ def scan_route_for_scenarios(route_description, world_annotations):
                     # can be instantiated later
 
                     if 'Antagonist_Vehicles' in location:
-                        other_vehicles = location['Antagonist_Vehicles']
+                        other_vehicles = remove_redundancy(location['Antagonist_Vehicles'])
                     else:
                         other_vehicles = None
 
-                    scenario_description = {'name': scenario_type,
-                                            'Antagonist_Vehicles': other_vehicles,
-                                            'waypoint': waypoint
-                                            }
+
+                    scenario_description ={'name': scenario_type,
+                                           'Antagonist_Vehicles': other_vehicles,
+                                           'waypoint': waypoint
+                                           }
 
                     possible_scenarios.append(scenario_description)
 
     return possible_scenarios
 
 
-def scenario_sampling(possible_scenarios):
-    # TODO sample the scenario following some criteria.
+def parse_trajectory(waypoints_trajectory):
 
-    return possible_scenarios
+    # Convert this to
+
+    return waypoints_trajectory, waypoints_trajectory
+
