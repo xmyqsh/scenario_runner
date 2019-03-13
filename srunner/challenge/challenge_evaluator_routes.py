e2e1f1e6b7ea07056f3f931b267572822838a7ea
Felipe Codevilla <codevilf@bart14.server.mila.quebec>
2019-03-12 20:37:40 -0400

    added the parsing part of the scenario runner

--- srunner/challenge/challenge_evaluator_routes.py
+++ srunner/challenge/challenge_evaluator_routes.py
@@ -15,17 +15,47 @@ import argparse
 from argparse import RawTextHelpFormatter
 import importlib
 import sys
+import os
+
+
+import xml.etree.ElementTree as ET
 
 #import carla
 import srunner.challenge.utils.route_configuration_parser as parser
-#from srunner.challenge.envs.server_manager import ServerManagerBinary, ServerManagerDocker
+from srunner.challenge.envs.server_manager import ServerManagerBinary, ServerManagerDocker
 #from srunner.scenarios.challenge_basic import *
 #from srunner.scenarios.config_parser import *
 #from srunner.scenariomanager.scenario_manager import ScenarioManager
 
+from srunner.scenarios.control_loss import ControlLoss
+from srunner.scenarios.follow_leading_vehicle import FollowLeadingVehicle
+from srunner.scenarios.object_crash_vehicle import StationaryObjectCrossing, DynamicObjectCrossing
+from srunner.scenarios.object_crash_intersection import VehicleTurningRight, VehicleTurningLeft
+from srunner.scenarios.opposite_vehicle_taking_priority import OppositeVehicleRunningRedLight
+from srunner.scenarios.signalized_junction_left_turn import SignalizedJunctionLeftTurn
+from srunner.scenarios.no_signal_junction_crossing import NoSignalJunctionCrossing
+# TODO maybe the parsing and the building are actually different.
 
-from
+# The configuration parser
 
+from srunner.scenarios.config_parser import ActorConfiguration
+
+number_class_translation = {
+
+
+    "Scenario 1": [ControlLoss],
+    "Scenario 2": [FollowLeadingVehicle],   # ToDO there is more than one class depending on the scenario configuraiton
+    "Scenario 3": [StationaryObjectCrossing, DynamicObjectCrossing],
+    "Scenario 4": [VehicleTurningRight, VehicleTurningLeft],
+    "Scenario 5": [],
+    "Scenario 6": [],
+    "Scenario 7": [OppositeVehicleRunningRedLight],
+    "Scenario 8": [SignalizedJunctionLeftTurn],
+    "Scenario 9": [],
+    "Scenario 10": [NoSignalJunctionCrossing]
+
+
+}
 
 
 # Import now all the possible scenarios
@@ -69,6 +99,27 @@ class ChallengeEvaluator(object):
         else:
             self._carla_server = ServerManagerBinary({'CARLA_SERVER': "{}/CarlaUE4.sh".format(args.carla_root)})
 
+    def cleanup(self, ego=False):
+        """
+        Remove and destroy all actors
+        """
+
+        # We need enumerate here, otherwise the actors are not properly removed
+        for i, _ in enumerate(self.actors):
+            if self.actors[i] is not None:
+                self.actors[i].destroy()
+                self.actors[i] = None
+        self.actors = []
+
+        for i, _ in enumerate(self._sensors_list):
+            if self._sensors_list[i] is not None:
+                self._sensors_list[i].destroy()
+                self._sensors_list[i] = None
+        self._sensors_list = []
+
+        if ego and self.ego_vehicle is not None:
+            self.ego_vehicle.destroy()
+            self.ego_vehicle = None
 
     def __del__(self):
         """
@@ -81,14 +132,67 @@ class ChallengeEvaluator(object):
         if self.world is not None:
             del self.world
 
-    def get_scenario_class_or_fail(self, scenario_type):
-        # TODO: probably should have  a class or module for itself.
-        # Factory method to get the proper class of the scenario
 
-        if scenario_type == 'Scenario1':
-            return ChallengeBasic
-        else:
-            return ChallengeBasic
+    def scenario_sampling(self, potential_scenarios_definitions):
+        return potential_scenarios_definitions
+
+    # convert to a better json
+    def get_actors_instances(self, list_of_antagonist_actors):
+
+
+        def get_actors_from_list(list_of_actor_def):
+            """
+                Receives a list of actor definitions and creates an actual list of ActorConfigurationObjects
+            :param list_of_actor_def:
+            :return:
+            """
+            def convert_json_to_actor(actor_dict):
+                node = ET.Element('waypoint')
+                node.set('x', actor_dict['x'])
+                node.set('y', actor_dict['y'])
+                node.set('z', actor_dict['z'])
+
+                return ActorConfiguration(node)
+
+            sublist_of_actors = []
+            for actor_def in list_of_actor_def:
+                sublist_of_actors.append(convert_json_to_actor(parser.create_location_waypoint(actor_def)))
+
+            return sublist_of_actors
+
+        list_of_actors = []
+
+        # Parse vehicles to the left
+        if 'Front' in list_of_antagonist_actors:
+            list_of_actors += get_actors_from_list(list_of_antagonist_actors['Front'])
+
+        if 'Left' in list_of_antagonist_actors:
+            list_of_actors += get_actors_from_list(list_of_antagonist_actors['Left'])
+
+        if 'Right' in list_of_antagonist_actors:
+
+            list_of_actors += get_actors_from_list(list_of_antagonist_actors['Right'])
+
+        return list_of_actors
+
+
+    def build_scenario_instances(self, scenario_definition_vec, town):
+        scenario_instance_vec = []
+
+        for definition in scenario_definition_vec:
+            # Get the class possibilities for this scenario number
+
+            posibility_vec = number_class_translation[definition['name']]
+            # for now I dont know how to disambiguate this part.
+            ScenarioClass = posibility_vec[0]
+
+            # Create the actors...
+            list_of_actor_conf_instances = self.get_actors_instances(definition['Antagonist_Vehicles'])
+
+            scenario_instance = ScenarioClass(self.world, self.ego_vehicle, list_of_actor_conf_instances, town)
+
+            scenario_instance_vec.append(scenario_instance)
+
 
     def run(self, args):
         """
@@ -100,22 +204,19 @@ class ChallengeEvaluator(object):
         # Which type of file is expected ????
         list_route_descriptions = parser.parse_routes_file(args.routes_file)
 
-
+        # For each of the routes to be evaluated.
         for route_description in list_route_descriptions:
             # find and filter potential scenarios
-            potential_scenarios = parser.scan_route_for_scenarios(route_description, world_annotations)
-            list_scenario_definitions = self.scenario_sampling(potential_scenarios)
+            potential_scenarios_definitions = parser.scan_route_for_scenarios(route_description, world_annotations)
+            list_of_scenarios_definitions = self.scenario_sampling(potential_scenarios_definitions)
 
             # prepare route's trajectory
             gps_route, world_coordinates_route = parser.parse_trajectory(route_description.trajectory)
 
             # pre-instantiate all scenarios for this route
             list_scenarios = [Idle()]
-            for scenario_definition in list_scenario_definitions:
-                Scenario = self.get_scenario_class_or_fail(scenario_definition['name'])
-                # properly instance the scenario.
-                scenario_inst = Scenario(scenario_definition)
-                list_scenarios.append(scenario_inst)
+            # build the instance based on the parsed definitions.
+            list_scenarios += self.build_scenario_instances(list_of_scenarios_definitions)
 
             # setup world and client assuming that the CARLA server is up and running
             client = carla.Client(args.host, int(args.port))
@@ -126,15 +227,15 @@ class ChallengeEvaluator(object):
             settings.synchronous_mode = True
             self.world.apply_settings(settings)
             client.tick()
+
             # create agent
             self.agent_instance = getattr(self.module_agent, self.module_agent.__name__)(args.config)
             self.agent_instance.set_global_plan(gps_route)
-
             # main loop
             while True:
                 # update all scenarios
                 for scenario in list_scenarios:
-                    scenario.scenario_tree.tick_once()
+                    scenario.scenario.scenario_tree.tick_once()
 
                 # ego vehicle acts
                 ego_action = self.agent_instance()
@@ -143,7 +244,16 @@ class ChallengeEvaluator(object):
                 # time continues
                 client.tick()
 
-            # statistics recording
+
+            self.cleanup(ego=True)
+        self.agent_instance.destroy()
+
+        self.final_summary(args)
+
+        # stop CARLA server
+        self._carla_server.stop()
+
+        # statistics recording
             # TODO:
 
             # cleanup
