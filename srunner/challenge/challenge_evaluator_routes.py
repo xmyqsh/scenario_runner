1046cf12bd0eb1a5d98d882717759126c57bbdf5
Felipe Codevilla <codevilf@bart14.server.mila.quebec>
2019-03-17 14:30:11 -0400

    Adding some prints

--- srunner/challenge/challenge_evaluator_routes.py
+++ srunner/challenge/challenge_evaluator_routes.py
@@ -58,7 +58,7 @@ def parse_trajectory(world, waypoints_trajectory):
     # INstantiate praveens full module
     # Setup the GRP
     hop_resolution = 2.0
-    print("going to start")
+    print("going to start ", world.get_map())
     dao = GlobalRoutePlannerDAO(world.get_map(), hop_resolution)
     grp = GlobalRoutePlanner(dao)
     grp.setup()
