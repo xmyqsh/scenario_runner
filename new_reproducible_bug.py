ead45a7012609350ab4f5e506804ca89b7b93ae1
Felipe Codevilla <codevilf@bart14.server.mila.quebec>
2019-03-18 14:32:21 -0400

    just a small test for bug

---
+++ new_reproducible_bug.py
@@ -0,0 +1,17 @@
+
+import carla
+
+client = carla.Client('localhost', int(2000))
+client.set_timeout(25.0)
+
+# For each of the routes to be evaluated.
+world = client.load_world('Town01')
+settings = world.get_settings()
+settings.synchronous_mode = True
+world.apply_settings(settings)
+
+
+world = client.load_world('Town02')
+settings = world.get_settings()
+settings.synchronous_mode = True
+world.apply_settings(settings)
