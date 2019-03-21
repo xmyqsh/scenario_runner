b235c6ecf63c471da466c9bb834ee751d9915a56
Felipe Codevilla <codevilf@bart14.server.mila.quebec>
2019-03-21 14:28:47 -0400

    remove synch mode for testing

--- load2maps.py
+++ load2maps.py
@@ -1,3 +1,4 @@
+"""
 import carla
 client = carla.Client('localhost', int(2000))
 client.set_timeout(25.0)
@@ -7,5 +8,22 @@ settings.synchronous_mode = True
 world.apply_settings(settings)
 
 world.tick()
+
+world = client.load_world('Town02')
+"""
+
+import carla
+client = carla.Client('localhost', int(2000))
+client.set_timeout(25.0)
+world = client.load_world('Town01')
+settings = world.get_settings()
+settings.synchronous_mode = True
+world.apply_settings(settings)
+
+world.tick()
+
+settings = world.get_settings()
+settings.synchronous_mode = False
+world.apply_settings(settings)
 
 world = client.load_world('Town02')
