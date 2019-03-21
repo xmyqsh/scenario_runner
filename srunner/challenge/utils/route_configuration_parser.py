661486410c07026356c5b94060762efb13041ce0
Felipe Codevilla <codevilf@bart14.server.mila.quebec>
2019-03-21 18:46:10 -0400

    route parser fix

--- srunner/challenge/utils/route_configuration_parser.py
+++ srunner/challenge/utils/route_configuration_parser.py
@@ -81,8 +81,12 @@ def scan_route_for_scenarios(route_description, world_annotations):
             dy = float(w1['y']) - wtransform.location.y
             dz = float(w1['z']) - wtransform.location.z
             dist_position = math.sqrt(dx * dx + dy * dy + dz * dz)
-            dyaw = float(w1['yaw']) - wtransform.rotation.pitch
-            dpitch = float(w1['pitch']) - wtransform.rotation.yaw
+            if  wtransform.rotation.pitch > 0:
+                dyaw = float(w1['yaw']) - wtransform.rotation.pitch
+                dpitch = float(w1['pitch']) - wtransform.rotation.yaw
+            else:
+                dyaw = float(w1['yaw']) - wtransform.rotation.yaw
+                dpitch = float(w1['pitch']) - wtransform.rotation.pitch
 
             dist_angle = math.sqrt(dyaw * dyaw + dpitch * dpitch)
             print (" dists ", dist_angle, dist_position)
