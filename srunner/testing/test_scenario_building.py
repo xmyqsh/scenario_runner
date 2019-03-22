5add5c3d5c9b526a4ac18d49ae22394da5c25fad
Felipe Codevilla <codevilf@bart14.server.mila.quebec>
2019-03-22 14:19:47 -0400

    testing scenario building

--- srunner/testing/test_scenario_building.py
+++ srunner/testing/test_scenario_building.py
@@ -53,34 +53,34 @@ class TestScenarioBuilder(unittest.TestCase):
         list_route_descriptions = parser.parse_routes_file(filename)
 
         # For each of the routes to be evaluated.
-        #for route_description in list_route_descriptions:
-        route_description = list_route_descriptions[0]
-        challenge.world = client.load_world(route_description['town_name'])
-        # Set the actor pool so the scenarios can prepare themselves when needed
-        CarlaActorPool.set_world(challenge.world)
-        # find and filter potential scenarios
-        # Returns the iterpolation in a different format
-        gps_route, route_description['trajectory'] = interpolate_trajectory(challenge.world,
-                                                                            route_description['trajectory'])
-        print (" FULL ROUTE ")
-        for wp in route_description['trajectory']:
+        for route_description in list_route_descriptions:
+            #route_description = list_route_descriptions[0]
+            challenge.world = client.load_world(route_description['town_name'])
+            # Set the actor pool so the scenarios can prepare themselves when needed
+            CarlaActorPool.set_world(challenge.world)
+            # find and filter potential scenarios
+            # Returns the iterpolation in a different format
+            gps_route, route_description['trajectory'] = interpolate_trajectory(challenge.world,
+                                                                                route_description['trajectory'])
+            #print (" FULL ROUTE ")
+            #for wp in route_description['trajectory']:
 
-            print (wp[0].transform.location)
+            #    print (wp[0].transform.location)
 
-        potential_scenarios_definitions = parser.scan_route_for_scenarios(route_description, world_annotations)
+            potential_scenarios_definitions = parser.scan_route_for_scenarios(route_description, world_annotations)
 
-        # prepare route's trajectory
-        challenge.prepare_ego_car(route_description['trajectory'][0][0].transform)
+            # prepare route's trajectory
+            challenge.prepare_ego_car(route_description['trajectory'][0][0].transform)
 
 
-        # build the master scenario based on the route and the target.
-        master_scenario = challenge.build_master_scenario(route_description['trajectory'], route_description['town_name'])
-        list_scenarios = [master_scenario]
-        print (" Built the master scenario ")
-        # build the instance based on the parsed definitions.
-        print (potential_scenarios_definitions)
-        list_scenarios += challenge.build_scenario_instances(potential_scenarios_definitions, route_description['town_name'])
+            # build the master scenario based on the route and the target.
+            master_scenario = challenge.build_master_scenario(route_description['trajectory'], route_description['town_name'])
+            list_scenarios = [master_scenario]
+            print (" Built the master scenario ")
+            # build the instance based on the parsed definitions.
+            print (potential_scenarios_definitions)
+            list_scenarios += challenge.build_scenario_instances(potential_scenarios_definitions, route_description['town_name'])
 
-        print (" Scenarios present ", list_scenarios)
+            print (" Scenarios present ", list_scenarios)
 
-        challenge.cleanup(ego=True)
+            challenge.cleanup(ego=True)
