0b8b684d9503024806e3e3b08d4ab357ad9e5238
Felipe Codevilla <codevilf@bart14.server.mila.quebec>
2019-03-22 15:53:52 -0400

    ego printing

--- srunner/scenarios/object_crash_intersection.py
+++ srunner/scenarios/object_crash_intersection.py
@@ -58,6 +58,8 @@ class VehicleTurningRight(BasicScenario):
         """
         waypoint = self._reference_waypoint
         _wp = generate_target_waypoint(waypoint, 1)
+
+        print (" Ego Transform ", self.ego_vehicle.get_transform() )
         offset = {"orientation": 270, "position": 90, "z": 0.2, "k": 0.7}
         _wp = _wp.next(10)[-1]
         lane_width = _wp.lane_width
@@ -165,7 +167,6 @@ class VehicleTurningLeft(BasicScenario):
         """
         Custom initialization
         """
-
         waypoint = self._reference_waypoint
         _wp = generate_target_waypoint(waypoint, -1)
         offset = {"orientation": 270, "position": 90, "z": 0.2, "k": 0.7}
