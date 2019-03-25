3cedb89a3fc81184edc9041c48f5bee1eab03cd8
felipecode <fcodevilla@cvc.uab.es>
2019-03-25 17:26:09 -0400

    printing for town04

--- srunner/testing/test_route_parser.py
+++ srunner/testing/test_route_parser.py
@@ -60,7 +60,7 @@ class TestRouteGenerator(unittest.TestCase):
 
         # For each of the routes to be evaluated.
         for route_description in list_route_descriptions:
-            if route_description['town_name'] == 'Town03':
+            if route_description['town_name'] == 'Town03' or route_description['town_name'] == 'Town01' :
                 continue
             print (" TOWN: ", route_description['town_name'])
             challenge.world = client.load_world(route_description['town_name'])
